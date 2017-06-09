4aceb3cf141aebedd33641f1bd2e605019040d11
xXKeyleXx <keyle@t-online.de>
2017-06-09 17:13:09 +0200

    make the skilltree loaded pseudo case insensitive

--- src/app/data/SkilltreeLoader.ts
+++ src/app/data/SkilltreeLoader.ts
@@ -23,21 +23,22 @@ import { Thorns } from "../models/skills/Thorns";
 import { Wither } from "../models/skills/Wither";
 import { Beacon } from "../models/skills/Beacon";
 import { logging } from "selenium-webdriver";
+import { MobTypes } from "./MobTypes";
 import Level = logging.Level;
 
 export class SkilltreeLoader {
   static loadSkilltree(data: any): Skilltree {
-    if (!data.Name || data.Name === "") {
+    if ((!data.Name && !data.name) || !(data.Name != "" || data.name != "")) {
       return null;
     }
-    let skilltree: Skilltree = {name: data.Name};
-    skilltree.permission = data.Permission || "";
-    skilltree.displayName = data.DisplayName || data.Name;
-    skilltree.description = data.Description || [];
+    let skilltree: Skilltree = {name: data.Name || data.Name};
+    skilltree.permission = data.permission || data.Permission || "";
+    skilltree.displayName = data.displayname || data.Displayname || data.displayName || data.DisplayName || skilltree.name;
+    skilltree.description = data.description || data.Description || [];
 
 
-    skilltree.skills = SkilltreeLoader.loadSkills(data.Skills);
-    skilltree.mobtypes = SkilltreeLoader.loadMobTypes(data.MobTypes);
+    skilltree.skills = SkilltreeLoader.loadSkills(data.skills || data.Skills);
+    skilltree.mobtypes = SkilltreeLoader.loadMobTypes(data.mobtypes || data.mobTypes || data.Mobtypes || data.MobTypes);
 
     return skilltree;
   }
@@ -46,7 +47,7 @@ export class SkilltreeLoader {
     let types = [];
     if (Array.isArray(data)) {
       data.forEach(name => {
-        if (Skills.indexOf(name) >= 0) {
+        if (MobTypes.indexOf(name) >= 0) {
           types.push(name)
         }
       })
@@ -59,7 +60,7 @@ export class SkilltreeLoader {
     Skills.forEach(skillInfo => {
       if (data[skillInfo.name]) {
         let upgrades = [];
-        Object.keys(data[skillInfo.name].Upgrades).forEach(key => {
+        Object.keys(data[skillInfo.name].upgrades || data[skillInfo.name].Upgrades).forEach(key => {
           let rule = SkilltreeLoader.loadLevelRule(key);
           let upgrade = SkilltreeLoader.SkillLoader[skillInfo.name](data[skillInfo.name].Upgrades[key]);
           upgrade.rule = rule;
@@ -96,7 +97,7 @@ export class SkilltreeLoader {
             if (!isNaN(modulo)) {
               levelRule.every = modulo;
             } else {
-              levelRule.every
+              levelRule.every = 1;
             }
             dynamic = true;
           } else if (match.startsWith(">")) {
@@ -133,16 +134,16 @@ export class SkilltreeLoader {
   static SkillLoader = {
     Backpack(data: any): Backpack {
       let backpack: Backpack = {};
-      backpack.rows = data.rows || 0;
-      backpack.drop = data.drop || null;
+      backpack.rows = data.rows || data.Rows || 0;
+      backpack.drop = data.drop || data.Drop || null;
       return backpack;
     },
 
     Beacon(data: any): Beacon {
       let beacon: Beacon = {};
-      beacon.range = data.Range || 0;
-      beacon.duration = data.Duration || 0;
-      beacon.count = data.Count || 0;
+      beacon.range = data.range || data.Range || 0;
+      beacon.duration = data.duration || data.Duration || 0;
+      beacon.count = data.count || data.Count || 0;
       if (data.Buffs) {
         beacon.buffs = {
           Absorption: {
@@ -198,127 +199,127 @@ export class SkilltreeLoader {
 
     Behavior(data: any): Behavior {
       let behavior: Behavior = {};
-      behavior.aggro = data.Aggro || null;
-      behavior.duel = data.Duel || null;
-      behavior.farm = data.Farm || null;
-      behavior.friend = data.Friend || null;
-      behavior.raid = data.Raid || null;
+      behavior.aggro = data.aggro || data.Aggro || null;
+      behavior.duel = data.duel || data.Duel || null;
+      behavior.farm = data.farm || data.Farm || null;
+      behavior.friend = data.friend || data.Friend || null;
+      behavior.raid = data.raid || data.Raid || null;
       return behavior;
     },
 
     Control(data: any): Control {
       let control: Control = {};
-      control.active = data.Active || null;
+      control.active = data.active || data.Active || null;
       return control;
     },
 
     Damage(data: any): Damage {
       let damage: Damage = {};
-      damage.damage = data.Damage || 0;
+      damage.damage = data.damage || data.Damage || 0;
       return damage;
     },
 
     Fire(data: any): Fire {
       let fire: Fire = {};
-      fire.chance = data.Chance || 0;
-      fire.duration = data.Duration || 0;
+      fire.chance = data.chance || data.Chance || 0;
+      fire.duration = data.duration || data.Duration || 0;
       return fire;
     },
 
     Heal(data: any): Heal {
       let heal: Heal = {};
-      heal.timer = data.Timer || 0;
-      heal.health = data.Health || 0;
+      heal.timer = data.timer || data.Timer || 0;
+      heal.health = data.health || data.Health || 0;
       return heal;
     },
 
     HealthBoost(data: any): HealthBoost {
       let healthBoost: HealthBoost = {};
-      healthBoost.health = data.Health || 0;
+      healthBoost.health = data.health || data.Health || 0;
       return healthBoost;
     },
 
     Knockback(data: any): Knockback {
       let knockback: Knockback = {};
-      knockback.chance = data.Chance || 0;
+      knockback.chance = data.chance || data.Chance || 0;
       return knockback;
     },
 
     Lightning(data: any): Lightning {
       let lightning: Lightning = {};
-      lightning.chance = data.Chance || 0;
-      lightning.damage = data.Damage || 0;
+      lightning.chance = data.chance || data.Chance || 0;
+      lightning.damage = data.damage || data.Damage || 0;
       return lightning;
     },
 
     Pickup(data: any): Pickup {
       let pickup: Pickup = {};
       pickup.range = data.Range || 0;
-      pickup.exp = data.EXP || null;
+      pickup.exp = data.exp || data.Exp || data.EXP || null;
       return pickup;
     },
 
     Poison(data: any): Poison {
       let poison: Poison = {};
-      poison.chance = data.Chance || 0;
-      poison.duration = data.Duration || 0;
+      poison.chance = data.chance || data.Chance || 0;
+      poison.duration = data.duration || data.Duration || 0;
       return poison;
     },
 
     Ranged(data: any): Ranged {
       let ranged: Ranged = {};
-      ranged.damage = data.Damage || 0;
-      ranged.rate = data.Rate || 0;
-      ranged.projectile = data.Projectile || "Arrow";
+      ranged.damage = data.damage || data.Damage || 0;
+      ranged.rate = data.rate || data.Rate || 0;
+      ranged.projectile = data.projectile || data.Projectile || "Arrow";
       return ranged;
     },
 
     Ride(data: any): Ride {
       let ride: Ride = {};
-      ride.speed = data.Speed || 0;
-      ride.jumpHeight = data.JumpHeight || 0;
-      ride.canFly = data.CanFly || null;
+      ride.speed = data.speed || data.Speed || 0;
+      ride.jumpHeight = data.jumpheight || data.Jumpheight || data.jumpHeight || data.JumpHeight || 0;
+      ride.canFly = data.canfly || data.Canfly || data.canFly || data.CanFly || null;
       return ride;
     },
 
     Shield(data: any): Shield {
       let shield: Shield = {};
-      shield.chance = data.Chance || 0;
-      shield.redirect = data.Redirect || 0;
+      shield.chance = data.chance || data.Chance || 0;
+      shield.redirect = data.redirect || data.Redirect || 0;
       return shield;
     },
 
     Slow(data: any): Slow {
       let slow: Slow = {};
-      slow.chance = data.Chance || 0;
-      slow.duration = data.Duration || 0;
+      slow.chance = data.chance || data.Chance || 0;
+      slow.duration = data.duration || data.Duration || 0;
       return slow;
     },
 
     Sprint(data: any): Sprint {
       let sprint: Sprint = {};
-      sprint.active = data.Active || 0;
+      sprint.active = data.active || data.Active || 0;
       return sprint;
     },
 
     Stomp(data: any): Stomp {
       let slow: Stomp = {};
-      slow.chance = data.Chance || 0;
-      slow.damage = data.Damage || 0;
+      slow.chance = data.chance || data.Chance || 0;
+      slow.damage = data.damage || data.Damage || 0;
       return slow;
     },
 
     Thorns(data: any): Thorns {
       let slow: Thorns = {};
-      slow.chance = data.Chance || 0;
-      slow.reflection = data.Reflection || 0;
+      slow.chance = data.chance || data.Chance || 0;
+      slow.reflection = data.reflection || data.Reflection || 0;
       return slow;
     },
 
     Wither(data: any): Wither {
       let slow: Wither = {};
-      slow.chance = data.Chance || 0;
-      slow.duration = data.Duration || 0;
+      slow.chance = data.chance || data.Chance || 0;
+      slow.duration = data.duration || data.Duration || 0;
       return slow;
     }
   }
