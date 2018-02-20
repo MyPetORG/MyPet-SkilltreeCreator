134e046749e7cc36f6e60256adfd13b9fe103ddb
xXKeyleXx <keyle@t-online.de>
2018-02-20 18:28:20 +0100

    NgModule import got deleted somehow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./components/app/app.component";
