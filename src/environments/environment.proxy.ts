4ff912037808ac9fd2d376c287c3a30407059aae
xXKeyleXx <github@keyle.de>
2018-03-21 10:42:25 +0100

    add proxy environment

---
+++ src/environments/environment.proxy.ts
@@ -0,0 +1,8 @@
+import { APP_VERSION } from "../version";
+
+export const environment = {
+  production: false,
+  providers: [],
+  websocketUrl: "localhost:64712",
+  version: "DEV-PROXY-" + APP_VERSION
+};
