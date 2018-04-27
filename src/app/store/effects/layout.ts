3e6056e9d9aa6be03e713beaa27f92b871cae6bc
xXKeyleXx <github@keyle.de>
2018-04-27 10:10:07 +0200

    some minor cleanup

--- src/app/store/effects/layout.ts
+++ src/app/store/effects/layout.ts
@@ -5,12 +5,11 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/skip';
 import 'rxjs/add/operator/takeUntil';
 import "rxjs/add/operator/withLatestFrom";
-import "rxjs/add/operator/withLatestFrom";
 import { Observable } from 'rxjs/Observable';
-import { tap, withLatestFrom } from "rxjs/operators";
+import { tap } from "rxjs/operators";
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Action, Store } from '@ngrx/store';
+import { Action } from '@ngrx/store';
 import * as LayoutActions from "../actions/layout";
 import { MatSnackBar } from "@angular/material";
 import { TranslateService } from "@ngx-translate/core";
@@ -18,14 +17,12 @@ import { languages } from "../../data/languages";
 import { defer } from "rxjs/observable/defer";
 import { of } from "rxjs/observable/of";
 import { WebsocketService } from "../../services/websocket.service";
-import * as Reducers from "../reducers";
 
 @Injectable()
 export class LayoutEffects {
   constructor(private actions$: Actions,
               private snackBar: MatSnackBar,
               private websocket: WebsocketService,
-              private store: Store<Reducers.State>,
               private translate: TranslateService) {
   }
 
