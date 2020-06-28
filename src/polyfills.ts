12a55203e9c8f6df933e838ccae0df55020c134a
xXKeyleXx <github@keyle.de>
2020-06-28 17:48:44 +0200

    ⬆️ upgrade dependencies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -19,11 +19,11 @@
  */
 
 /** IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es6/reflect';
-import 'core-js/es7/array';
+// import 'core-js/es6/reflect';
+// import 'core-js/es7/array';
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect';
+// import 'core-js/es7/reflect';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
