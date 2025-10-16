5782cdf82950f2dda9be35d2e651e77a14a66ba4
UserDerezzed <me@userderezzed.dev>
2025-10-16 05:31:56 -0700

    Update vite.config.ts to fix building

--- vite.config.ts
+++ vite.config.ts
@@ -17,10 +17,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-const isCI = !!process.env.GITHUB_ACTIONS
-
 export default defineConfig({
-    plugins: [react()],
-    base: isCI && repoName ? `/${repoName}/` : '/',
+  plugins: [react()],
+  base: '/',
 })
